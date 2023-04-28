fe90a60c763bf39d0360007ff58822dee80a2692
MathSauc3 <732584@student.dusd.net>
2023-04-27 21:02:59 -0700

    Finished 5

--- UserInterfaceSaucedo/Assets/Scripts/GameManager.cs
+++ UserInterfaceSaucedo/Assets/Scripts/GameManager.cs
@@ -17,6 +17,8 @@ public class GameManager : MonoBehaviour
     public GameObject titleScreen;
     public TextMeshProUGUI livesText;
     private int lives;
+    public GameObject pauseScreen;
+    private bool paused;
     // Start is called before the first frame update
     void Start()
     {
@@ -42,6 +44,22 @@ public class GameManager : MonoBehaviour
         StartCoroutine(SpawnTarget());
         UpdateScore(0);
         UpdateLives(3);
+    }
+
+    void ChangePaused()
+    {
+        if(!paused)
+        {
+            paused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            paused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 
     public void RestartGame()
@@ -75,6 +93,9 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangePaused();
+        }
     }
 }
